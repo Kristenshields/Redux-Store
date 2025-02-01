0621760193530d1b295569bf7eceff43b08524e9
Kristen Shields <shieldskristen0@gmail.com>
2025-02-01 12:47:11 -0700

    updated server code

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,40 +1,43 @@
-const { GraphQLError } = require('graphql');
-const jwt = require('jsonwebtoken');
+import { GraphQLError } from 'graphql';
+import jwt from 'jsonwebtoken';
+import 'dotenv/config';
 
-const secret = 'mysecretssshhhhhhh';
+const secret = process.env.JWT_SECRET || '';
 const expiration = '2h';
 
-module.exports = {
-  AuthenticationError: new GraphQLError('Could not authenticate user.', {
-    extensions: {
-      code: 'UNAUTHENTICATED',
-    },
-  }),
-  authMiddleware: function ({ req }) {
-    // allows token to be sent via req.body, req.query, or headers
-    let token = req.body.token || req.query.token || req.headers.authorization;
+export const AuthenticationError = new GraphQLError('Could not authenticate user.', {
+  extensions: {
+    code: 'UNAUTHENTICATED',
+  },
+});
 
-    // ["Bearer", "<tokenvalue>"]
-    if (req.headers.authorization) {
-      token = token.split(' ').pop().trim();
-    }
+export const authMiddleware = function ({ req }) {
+  // Allow token from multiple sources
+  let token = req.body.token || req.query.token || req.headers.authorization;
 
-    if (!token) {
-      return req;
-    }
+  // Clean up Bearer token
+  if (req.headers.authorization) {
+    token = token.split(' ').pop().trim();
+  }
 
-    try {
-      const { data } = jwt.verify(token, secret, { maxAge: expiration });
-      req.user = data;
-    } catch {
-      console.log('Invalid token');
-    }
-
+  if (!token) {
     return req;
-  },
-  signToken: function ({ firstName, email, _id }) {
-    const payload = { firstName, email, _id };
+  }
 
-    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
-  },
+  try {
+    // Verify token and attach user data to request
+    const { data } = jwt.verify(token, secret, { maxAge: expiration });
+    req.user = data;
+  } catch {
+    console.log('Invalid token!');
+    throw new AuthenticationError('Invalid token!');
+  }
+
+  return req;
+};
+
+export const signToken = function ({ firstName, email, _id }) {
+  const payload = { firstName, email, _id };
+  
+  return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
 };
